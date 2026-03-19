c98381830b562040a414427711af82f681992224
laetitiamartinez-ops <laetitia.martinez@chronopneus.fr>
2026-03-19 16:13:02 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "transport-pro-v1";
+const CACHE_NAME = "transport-pro-v2";
 const ASSETS = ["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).catch(() => {}));
